4a377402813e7bbfe79aabc12ce9e09c8eb5ed2c
Maya Hampton <mhampton2003@gmail.com>
2022-09-22 11:42:07 -0700

    first

---
+++ src/SLL.java
@@ -0,0 +1,70 @@
+
+public class SLL {
+
+	Node head;
+	
+	static class Node{
+		int data;
+		Node next;
+		
+		Node(int d) {
+			data = d;
+			next = null;
+		}
+	}
+	
+	//default constructor
+	public SLL() {
+		
+	}
+	
+	//copy constructor
+	public SLL(SLL aList) {
+		
+	}
+	
+	//accessing first element
+	public void front() {
+		
+	}
+	
+	//accessing last element
+	public void back() {
+		
+	}
+	
+	//insert value
+	public void insert(int val) {
+		
+	}
+	
+	//remove value at front
+	public void pop_front() {
+		
+	}
+	
+	//remove value at end
+	public void pop_back() {
+		
+	}
+
+	//determines if empty
+	public void empty() {
+		
+	}
+	
+	//returns number of elements
+	public int size() {
+		return -1;
+	}
+	
+	//reverses order of elements in the list
+	public void reverse() {
+		
+	}
+	
+	//merge with another ordered list
+	public void merge(SLL aList) {
+		
+	}
+}
