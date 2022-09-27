19ccb07e93ba49119e806be6afc6d172280e9b36
mhampton2003 <74636177+mhampton2003@users.noreply.github.com>
2022-09-26 23:08:40 -0700

    SLL base

--- src/SLL.java
+++ src/SLL.java
@@ -2,6 +2,7 @@
 public class SLL {
 
 	Node head;
+	int counter;
 	
 	static class Node{
 		int data;
@@ -15,56 +16,117 @@ public class SLL {
 	
 	//default constructor
 	public SLL() {
-		
+		head = null;
 	}
 	
 	//copy constructor
 	public SLL(SLL aList) {
-		
+		Node temp = new Node(head.data);
+		temp = head;
 	}
 	
 	//accessing first element
 	public void front() {
-		
+		System.out.println(head.data);
 	}
 	
 	//accessing last element
 	public void back() {
+		Node temp = new Node(head.data);
+		temp = head;
 		
+		for (int i = 0; i < counter - 1; i++) {
+			temp = temp.next;
+		}
+		System.out.println(temp.data);
 	}
 	
 	//insert value
 	public void insert(int val) {
+		if (head == null) {
+			head = new Node(val);
+			counter++;
+		}
 		
+		else {
+			Node newNode = new Node(val);
+			newNode.next = head;
+			head = newNode;
+		
+			counter++;
+		}
 	}
 	
 	//remove value at front
 	public void pop_front() {
-		
+		Node temp = new Node(head.data);
+		temp = head;
+		temp = temp.next;
+		head = temp;
+		counter--;
 	}
 	
 	//remove value at end
 	public void pop_back() {
+		Node temp = new Node(head.data);
+		temp = head;
 		
+		for (int i = 0; i < counter - 1; i++) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+		
+		
+		temp = null;
+		counter--;
+
 	}
 
 	//determines if empty
-	public void empty() {
-		
+	public Boolean empty() {
+		if (head == null) {
+			return true;
+		}
+		else {
+			return false;	
+		}
 	}
 	
 	//returns number of elements
 	public int size() {
-		return -1;
+		return counter;
 	}
 	
 	//reverses order of elements in the list
 	public void reverse() {
-		
+		SLL sll2 = new SLL();
+		Node temp = new Node(head.data);
+		temp = head;
+				
+		for (int i = counter; i > 0; i--) {
+			sll2.insert(temp.data);
+			temp = temp.next;
+		}
+		sll2.print();
 	}
 	
 	//merge with another ordered list
 	public void merge(SLL aList) {
-		
+		Node temp = new Node(head.data);
+		temp = head;
+				
+		for (int i = counter; i > 0; i--) {
+			aList.insert(temp.data);
+			temp = temp.next;
+		}
+		aList.print();
+	}
+	
+	public void print() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
 }
